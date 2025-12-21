006403046055e5ae2772776db8dd6f5cd12105a0
sir-abbey <kaybiel4u@gmail.com>
2025-12-21 08:33:13 +0100

    Creating Entities and Dtos.

--- backend/src/main/java/com/abiodunelijah/review/entities/Review.java
+++ backend/src/main/java/com/abiodunelijah/review/entities/Review.java
@@ -1,10 +1,14 @@
 package com.abiodunelijah.review.entities;
 
+import com.abiodunelijah.auth_users.entities.User;
+import com.abiodunelijah.menu.entities.Menu;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "menus")
@@ -17,4 +21,21 @@ public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",  nullable = false)
+    private User user;
+
+    private Integer rating;
+
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+    private LocalDateTime createdAt;
+
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
 }
