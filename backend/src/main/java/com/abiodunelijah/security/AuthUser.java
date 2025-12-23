9ff43f9edb551a4314fd19bcb775f2968bcbe664
sir-abbey <kaybiel4u@gmail.com>
2025-12-23 05:50:45 +0100

    Handling Authentication And Authorization

---
+++ backend/src/main/java/com/abiodunelijah/security/AuthUser.java
@@ -0,0 +1,42 @@
+package com.abiodunelijah.security;
+
+import com.abiodunelijah.auth_users.entities.User;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+public class AuthUser implements UserDetails {
+
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles()
+                .stream()
+                .map(role-> new SimpleGrantedAuthority(role.getName()))
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+
+    @Override
+    public boolean isEnabled() {
+        return user.isActive();
+    }
+}
