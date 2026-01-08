f4e9f5fc03a6bd19fccf1d02c086e39b2261baa3
sir-abbey <kaybiel4u@gmail.com>
2026-01-08 11:49:00 +0100

    Handling Frontend module

---
+++ frontend/src/services/Guard.jsx
@@ -0,0 +1,13 @@
+import {Component} from "react";
+import ApiService from "./ApiService.jsx";
+import {Navigate, useLocation} from "react-router-dom";
+
+export const CustomerRoute = ({element: Component}) =>{
+    const location = useLocation()
+    return ApiService.isAdmin()? (Component) : (<Navigate to={"/login"} replace state={{from: location}}/>)
+}
+
+export const AdminRoute = ({element: Component}) =>{
+    const location = useLocation()
+    return ApiService.isAdmin()? (Component) : (<Navigate to={"/login"} replace state={{from: location}}/>)
+}
